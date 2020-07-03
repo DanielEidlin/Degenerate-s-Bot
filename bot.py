ad3ca2e2ccea42a26fec1357c52c374f11e07af2
Daniel Eidlin <daniel.eidlin@gmail.com>
2020-07-03 14:31:22 +0300

    Display score automatically

--- bot.py
+++ bot.py
@@ -3,6 +3,10 @@ from discord.ext import commands
 client = commands.Bot(command_prefix='#')
 bot_token = 'NzI4MjI2OTQwMTM3MjQyNjk3.Xv5oRw.3iWs7qFxPU2VNasbUqErc8rz_gM'
 user_score = {}
+"""
+A dictionary that contains the user nickname as the key and his score and a state of vote as the value.
+{nickname: [score, voted]
+"""
 
 
 def get_member_by_letter(letter):
@@ -26,7 +30,7 @@ async def on_ready():
         await guild.text_channels[0].send("BRACE YOURSELVES DEGENERATES! IT'S SHOW TIME!!!!")
 
     global user_score
-    user_score = {member.nick: 0 for member in client.get_all_members() if
+    user_score = {member.nick: [0, False] for member in client.get_all_members() if
                   "Waifu Protector" in [role.name for role in member.roles]}
 
 
@@ -91,10 +95,22 @@ async def vote(ctx, user_vote):
     :return: None
     """
     user = ctx.author
-    voted_user = get_member_by_letter(user_vote[0])
-    user_score[voted_user] += 1
+    user_score[user.nick][1] = True     # Update user's vote state to True.
+    voted_user = get_member_by_letter(user_vote[0])     # Get the chosen user.
+    user_score[voted_user][0] += 1     # Increment score of the chosen user.
     print(f"{user} your vote has been registered!")
-    await ctx.send(f"{user} your vote has been registered!")
+
+    # Check if all users have voted.
+    for member in user_score:
+        if user_score[member][1] is False:
+            return
+
+    # Update all users' vote state to False.
+    for member in user_score:
+        user_score[member][1] = False
+
+    # Send score.
+    await ctx.send(f"Here is the score:\n{user_score}")
 
 
 @client.command(pass_context=True)
